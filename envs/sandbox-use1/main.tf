df52c1d34a99d24ca20d856b84368f473080486e
Andre Silva <andreswebs@pm.me>
2025-09-02 16:21:21 -0300

    upd

--- envs/sandbox-use1/main.tf
+++ envs/sandbox-use1/main.tf
@@ -1,12 +1,7 @@
-locals {
-  vpn_cidr = "10.0.0.0/8"
-}
-
 module "ec2_standalone" {
-  source              = "../../modules/instance-standalone"
-  name                = var.name
-  instance_type       = var.instance_type
-  ami_id              = var.ami_id
-  subnet_id           = var.subnet_id
-  cidr_whitelist_ipv4 = [local.vpn_cidr]
+  source        = "../../modules/instance-standalone"
+  name          = var.name
+  instance_type = var.instance_type
+  ami_id        = var.ami_id
+  subnet_id     = var.subnet_id
 }
